91694d5dadc40a8b8fbe9599181858469a4e7636
xit4 <v.labalestra@gmail.com>
2014-12-10 14:31:52 +0100

    best comment ever

--- src/com/example/parte/MainActivity.java
+++ src/com/example/parte/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
+    // TORIJEAONPA HBFKOA GJFJAKG C HJVCVHJIP HC
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
