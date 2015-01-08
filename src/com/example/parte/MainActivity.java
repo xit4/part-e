cd8a51df8ffa766653cdd79884ab29e09ce12aaf
Matteo <Matteo@matteo-pc>
2015-01-08 11:24:11 +0100

    Added material create party button, position is wrong, updated manifest to min 21 API (material design)

--- src/com/example/parte/MainActivity.java
+++ src/com/example/parte/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -54,6 +55,13 @@ public class MainActivity extends Activity {
 			join.setOnClickListener(new View.OnClickListener() {
 				public void onClick(View v) {
 					joinParty();
+				}
+			});
+			
+			final ImageButton fab = (ImageButton) findViewById(R.id.fab);
+			fab.setOnClickListener(new View.OnClickListener() {
+				public void onClick(View v) {
+					createParty();
 				}
 			});
 		}
